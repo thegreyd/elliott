01fa6b76dd26078092c87ee125ebe4abbaf08a2e
Siddharth Sharma <sidsharm@redhat.com>
2021-06-24 16:00:59 -0400

    enhance golang cmd to accept nvrs

--- elliottlib/cli/get_golang_versions.py
+++ elliottlib/cli/get_golang_versions.py
@@ -1,7 +1,6 @@
-from elliottlib import brew, errata, rhcos, cincinnati
+from elliottlib import brew, errata, rhcos, cincinnati, util
 from elliottlib.cli.common import cli
 from elliottlib.exceptions import BrewBuildException
-from elliottlib.util import get_golang_version_from_root_log, red_print
 
 import click
 import functools
@@ -41,8 +40,10 @@ def get_container_golang_versions(advisory_id: str):
 @cli.command("get-golang-versions", short_help="Get version of Go for advisory builds")
 @click.option('--advisory', '-a', 'advisory_id',
               help="The advisory ID to fetch builds from")
+@click.option('--nvrs', '-n',
+              help="Brew nvrs to show go version for. Comma separated")
 @click.pass_obj
-def get_golang_versions_cli(runtime, advisory_id, ocp_pullspec, latest, latest_ocp, packages, arch):
+def get_golang_versions_cli(runtime, advisory_id, nvrs):
     """
     Prints the Go version used to build a component to stdout.
 
@@ -51,13 +52,12 @@ def get_golang_versions_cli(runtime, advisory_id, ocp_pullspec, latest, latest_o
 \b
     $ elliott get-golang-versions -a ID
 
-"""
-    count_options = sum(map(bool, [advisory_id, ocp_pullspec, latest, latest_ocp]))
-    if count_options > 1:
-        raise click.BadParameter("Use only one of --advisory, --rhcos, --rhcos-latest, --rhcos-ocp")
+\b
+    $ elliott get-golang-versions -n podman-1.9.3-3.rhaos4.6.el8
 
-    if arch and not (latest or latest_ocp):
-        raise click.BadParameter("--arch can only be used with --rhcos-latest, --rhcos-ocp")
+"""
+    if advisory_id and nvrs:
+        raise click.BadParameter("Use only one of --advisory, --nvrs")
 
     if advisory_id:
         content_type = errata.get_erratum_content_type(advisory_id)
@@ -65,36 +65,12 @@ def get_golang_versions_cli(runtime, advisory_id, ocp_pullspec, latest, latest_o
             get_container_golang_versions(advisory_id)
         else:
             get_rpm_golang_versions(advisory_id)
+        return
     
-    runtime.initialize()
-    major = runtime.group_config.vars.MAJOR
-    minor = runtime.group_config.vars.MINOR
-    version = f'{major}.{minor}'
-
-    build_id = ''
-    arch = 'x86_64' if not arch else arch
-    
-    if latest or latest_ocp:
-        if latest:
-            print(f'Looking up latest rhcos build id for {version} {arch}')
-            build_id = rhcos.latest_build_id(version, arch)
-            print(f'Build id found: {build_id}')
-        else:
-            print(f'Looking up last ocp release for {version} {arch}')
-            release = cincinnati.get_latest_stable_ocp(version, arch)
-            if not release:
+    if nvrs:
+        nvrs = [n.strip().split('-') for n in nvrs.split(',')]
+        for n in nvrs:
+            if len(n) < 3:
+                print(f'Invalid nvr: {n}')
                 return
-            print(f'OCP release: {release}')
-            ocp_pullspec = f'quay.io/openshift-release-dev/ocp-release:{release}-{arch}'
-    
-    if ocp_pullspec:
-        print(f"Looking up rhcos build id for {ocp_pullspec}")
-        build_id, arch = rhcos.get_build_from_payload(ocp_pullspec)
-        print(f'Build id found: {build_id}')
-    
-    if build_id:
-        nvrs = rhcos.get_rpm_nvrs(build_id, version, arch)
-        if packages:
-            packages = [p.strip() for p in packages.split(',')]
-            nvrs = [p for p in nvrs if p[0] in packages]
-        get_rpm_golang_from_nvrs(nvrs)
+    util.get_rpm_golang_from_nvrs(nvrs)
