1fb91017608f0b0bb5c09de6325f447c8bbd5f41
Siddharth Sharma <sidsharm@redhat.com>
2021-07-01 14:02:09 -0400

    fixes + get nvr working

--- elliottlib/cli/get_golang_versions.py
+++ elliottlib/cli/get_golang_versions.py
@@ -1,40 +1,7 @@
-from elliottlib import brew, errata, rhcos, cincinnati, util
+from elliottlib import brew, errata, util
 from elliottlib.cli.common import cli
-from elliottlib.exceptions import BrewBuildException
-
+from kobo.rpmlib import parse_nvr
 import click
-import functools
-import semver
-import urllib
-import json
-
-
-def get_rpm_golang_versions(advisory_id: str):
-    advisory_nvrs = errata.get_all_advisory_nvrs(advisory_id)
-    click.echo(f"Found {len(advisory_nvrs)} builds in advisory {advisory_id}")
-    print(advisory_nvrs)
-    brew.get_rpm_golang_from_nvrs(advisory_nvrs)
-
-
-def get_container_golang_versions(advisory_id: str):
-    all_builds = errata.get_brew_builds(advisory_id)
-
-    all_build_objs = brew.get_build_objects([b.nvr for b in all_builds])
-    for build in all_build_objs:
-        golang_version = None
-        name = build.get('name')
-        try:
-            parents = build['extra']['image']['parent_image_builds']
-        except KeyError:
-            print('Could not get parent image info for {}'.format(name))
-            continue
-
-        for p, pinfo in parents.items():
-            if 'builder' in p:
-                golang_version = pinfo.get('nvr')
-
-        if golang_version is not None:
-            print('{}:\t{}'.format(name, golang_version))
 
 
 @cli.command("get-golang-versions", short_help="Get version of Go for advisory builds")
@@ -43,34 +10,49 @@ def get_container_golang_versions(advisory_id: str):
 @click.option('--nvrs', '-n',
               help="Brew nvrs to show go version for. Comma separated")
 @click.pass_obj
-def get_golang_versions_cli(runtime, advisory_id, nvrs):
+def get_golang_versions_cli(advisory_id, nvrs):
     """
     Prints the Go version used to build a component to stdout.
 
     Usage:
 
 \b
-    $ elliott get-golang-versions -a ID
+    $ elliott get-golang-versions -a 76557
+
+    List brew builds attached to the advisory with their go version
 
 \b
-    $ elliott get-golang-versions -n podman-1.9.3-3.rhaos4.6.el8
+    $ elliott get-golang-versions -n podman-3.0.1-6.el8,podman-1.9.3-3.rhaos4.6.el8
 
+    List given brew builds with their go version
 """
     if advisory_id and nvrs:
         raise click.BadParameter("Use only one of --advisory, --nvrs")
 
     if advisory_id:
+        advisory_nvrs = errata.get_all_advisory_nvrs(advisory_id)
+        click.echo(f"Found {len(advisory_nvrs)} builds in advisory {advisory_id}")
+        print(advisory_nvrs)
+
         content_type = errata.get_erratum_content_type(advisory_id)
         if content_type == 'docker':
-            get_container_golang_versions(advisory_id)
+            util.get_golang_from_nvrs(advisory_nvrs)
         else:
-            get_rpm_golang_versions(advisory_id)
+            util.get_golang_from_nvrs(advisory_nvrs, rpm=True)
         return
 
     if nvrs:
-        nvrs = [n.strip().split('-') for n in nvrs.split(',')]
+        nvrs = [n.strip() for n in nvrs.split(',')]
+        container_nvrs, rpm_nvrs = [], []
         for n in nvrs:
-            if len(n) < 3:
-                print(f'Invalid nvr: {n}')
-                return
-    util.get_rpm_golang_from_nvrs(nvrs)
+            parsed_nvr = parse_nvr(n)
+            nvr_tuple = (parsed_nvr['name'], parsed_nvr['version'], parsed_nvr['release'])
+            if 'container' in parsed_nvr['name']:
+                container_nvrs.append(nvr_tuple)
+            else:
+                rpm_nvrs.append(nvr_tuple)
+        if rpm_nvrs:
+            util.get_golang_from_nvrs(rpm_nvrs, rpm=True)
+        if container_nvrs:
+            util.get_golang_from_nvrs(container_nvrs)
+        return
--- elliottlib/util.py
+++ elliottlib/util.py
@@ -315,27 +315,45 @@ def get_golang_version_from_root_log(root_log):
     return s
 
 
-def get_rpm_golang_from_nvrs(nvrs):
-    go_fail, brew_fail = 0, 0
-    for nvr in nvrs:
-        try:
-            root_log = brew.get_nvr_root_log(*nvr)
-        except BrewBuildException:
-            # print(e)
-            brew_fail += 1
-            continue
-        try:
-            golang_version = get_golang_version_from_root_log(root_log)
-        except AttributeError:
-            # print('Could not find Go version in {}-{}-{} root.log'.format(*nvr))
-            go_fail += 1
-            continue
-        nvr_s = '{}-{}-{}'.format(*nvr)
-        print(f'{nvr_s} {golang_version}')
-    if go_fail:
-        print(f'Could not find Go version in Brew build log for {go_fail} nvrs')
-    if brew_fail:
-        print(f'Could not get Brew build log for {brew_fail} nvrs')
+def get_golang_from_nvrs(nvrs, rpm=False):
+    if not rpm:
+        all_build_objs = brew.get_build_objects(nvrs)
+        for build in all_build_objs:
+            golang_version = None
+            name = build.get('name')
+            try:
+                parents = build['extra']['image']['parent_image_builds']
+            except KeyError:
+                print('Could not get parent image info for {}'.format(name))
+                continue
+
+            for p, pinfo in parents.items():
+                if 'builder' in p:
+                    golang_version = pinfo.get('nvr')
+
+            if golang_version is not None:
+                print('{}:\t{}'.format(name, golang_version))
+    if rpm:
+        go_fail, brew_fail = 0, 0
+        for nvr in nvrs:
+            try:
+                root_log = brew.get_nvr_root_log(*nvr)
+            except BrewBuildException:
+                # print(e)
+                brew_fail += 1
+                continue
+            try:
+                golang_version = get_golang_version_from_root_log(root_log)
+            except AttributeError:
+                # print('Could not find Go version in {}-{}-{} root.log'.format(*nvr))
+                go_fail += 1
+                continue
+            nvr_s = '{}-{}-{}'.format(*nvr)
+            print(f'{nvr_s} {golang_version}')
+        if go_fail:
+            print(f'Could not find Go version in Brew build log for {go_fail} nvrs')
+        if brew_fail:
+            print(f'Could not get Brew build log for {brew_fail} nvrs')
 
 
 # some of our systems refer to golang's architecture nomenclature; translate between that and brew arches
